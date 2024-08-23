b19168a50ed61c04de39758fe43a9ea7c38f82e0
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2024-08-23 16:47:12 +0300

    Homework-JS-23

--- main.js
+++ main.js
@@ -1,67 +1,40 @@
-//! Задача1
-// let i = 0;
-// for (i=0;i <=100;i++) {
-//     console.log(i);
-// }
+const fruits = ["strawbery", "mango", "watermelon", "grapes", "Lemon", "Kiwi"]
+console.log(fruits);
+fruits.sort();
+console.log(fruits);
 
-//! Задача2
-// let num = 0
-// while(num < 100) {
-//     num++;
-//     if(num % 2 == 0) console.log(num)
-// }
+const user = ["strawbery", "mango", "watermelon", "grapes", "Lemon", "Kiwi"]
+console.log(user);
+user.sort().reverse();
+console.log(user);
 
-//! Задача3
-// let x = 0;
-// for (let i=0;i<=100;i++) {
-//     if (i % 2 == 0) {
-//         x = x+i
-//     }
-// }
-// console.log(x)
+const grades = [4, 4, 4, 5, 3, 5, 3, 3, 2, 5, 1];
+console.log(grades);
+grades.sort();
+console.log(grades);
 
-//! Задача4
-    //?  "-"
-
-//! Задача5
-//  let text = ("hello, i'm Nurmukhamat!");
-//  text = text.split("").reverse().join("");
-//  console.log(text);
- 
-//! Задача6
-// let Number = prompt("введите код");
-// for (let key = 0; key <= Number; key++) {
-//   console.log(key);
-// }
-
-// let line = 7;
-// let line2 = "";
-// let line3 = "*";
-// for (let ts = 0; ts < line; ts++) {
-//   line2 += line3;
-//   console.log(line2);
-//   }
-//   let wl = "7";
-//   let res1 = "";
-//   let res2 = "#";
-//   let res3 = "0";
-
-//   while (wl >= res3) {
-//     res1 += res2;
-//     console.log(res1);
-//     res3++
-//    }
-
-//! Задача7
-// var golem = "#";
-// while (golem.length<=7){
-//     console.log(golem + "\n");
-//     golem+="#";
-// }
-
-// function eren() {
-//     for (let naruto = "@"; naruto.length < 8; naruto += "@") {
-//       console.log(naruto);
-//     }
-//   }
-//   eren();
+const computers = [
+  {
+    title: "Macbook Air Al",
+    price: 85000,
+  },
+  {
+    title: "Dell",
+    price: "65000",
+  },
+  {
+    title: "Asus ZenBook",
+    price: "40000",
+  },
+  {
+    title: "Acer Aspire",
+    price: 45000,
+  },
+  {
+    title: "Macbook Pro",
+    price: 100000,
+  },
+];
+console.log(computers);
+computers.sort().reverse
+console.log(computers);
