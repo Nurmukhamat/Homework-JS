05a739a9fdf5bd5e79d981922b3f6e2949bb475d
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2024-08-23 16:32:25 +0300

    Homework-JS-18

--- main.js
+++ main.js
@@ -1,98 +1,67 @@
-// ! Задача1
-// const num = prompt("enter your number!");
-// if (num == 10) {
-//     console.log(true)
-// } else  {
-//     console.log(false);
-// };
-
-// ! Задача2
-// const num2 = prompt("Введите номер квартиры!");
-// if (num2 > 0 && num2 <= 20) {
-//     console.log("Ваша квартира находится в 1 подъезде"); 
-// }
-//  else if (num2 > 20 && num2 <= 48) {
-//     console.log("Ваша квартира находится в 2 подъезде"); 
-// }
-//  else if (num2 > 48 && num2 <= 90) {
-//     console.log("Ваша квартира находится в 3 подъезде"); 
-// } else {
-//     console.log("Вы ошиблись квартирой!");
+//! Задача1
+// let i = 0;
+// for (i=0;i <=100;i++) {
+//     console.log(i);
 // }
 
-// ! Задача3
-// const num3 = prompt("Введите ваше число");
-// if ( num3 > 0 && num3 < 5) {
-//     console.log(true);
-// }
-// else {
-//     console.log(false);
+//! Задача2
+// let num = 0
+// while(num < 100) {
+//     num++;
+//     if(num % 2 == 0) console.log(num)
 // }
 
-// !Задача4
-// const hours = prompt("Введите текущий час")
-// if (hours > 6 && hour <= 12) console.log("Доброй утро");
-// if (hours > 12 && hour <= 17) console.log("Доброе день");
-// if (hours > 17 && hour <= 24) console.log("Добрый ночи");
-// if (hours > 0 && hour <= 6) console.log("Добрый ночи");
+//! Задача3
+// let x = 0;
+// for (let i=0;i<=100;i++) {
+//     if (i % 2 == 0) {
+//         x = x+i
+//     }
+// }
+// console.log(x)
 
-// ! Задача5
-// const hour = prompt("Введите текущий час");
-// const userName = prompt("Как вас зовут?");
-// if (hour > 6 && hour <= 12) console.log("Доброе утро " + userName);
-// if (hour > 12 && hour <= 17) console.log("Добрый день "+userName);
-// if (hour > 17 && hour <= 24) console.log("Доброй ночи "+ userName);
-// if (hour > 0 && hour <= 6) console.log("Доброй  ночи "+ userName);
+//! Задача4
+    //?  "-"
 
-// ! Задача6
-// const x = prompt("Сколько вам лет?");
-// if (x <= 6 || x >= 65) {
-//     console.log("для пенсионерам и детям действует скида 50%!");
-// } else {
-//     console.log("Оплатите наличкой или картой?");
-// }
-
-// !Задача7
-// let user1 = prompt("ИГРОК1) выберите вариант: 1)Камень, 2)Ножницы, 3)Бумага");
-// let user2 = prompt("ИГРОК2) выберите вариант: 1)Камень, 2)Ножницы, 3)Бумага");
-// if (user1 == 1 && user2 == 1) {
-//     console.log("ничья");
-// }
-// if (user1 == 1 && user2 == 2) {
-//     console.log("победил ИГРОК1");
-// }
-// if (user1 == 1 && user2 == 3) {
-//     console.log("Победил ИГРОК2");
-// }
-// if (user1 == 2 && user2 == 1) {
-//     console.log("Победил ИГРОК2");
-// }
-// if (user1 == 2 && user2 == 2) {
-//     console.log("ничья");
-// }
-// if (user1 == 2 && user2 == 3) {
-//     console.log("победил ИГРОК1");
-// }
-// if (user1 == 3 && user2 == 1) {
-//     console.log("победил ИГРОК1");
-// }
-// if (user1 == 3 && user2 == 2) {
-//     console.log("Победил ИГРОК2");
-// }
-// if (user1 == 3 && user2 == 3) {
-//     console.log("ничья");
+//! Задача5
+//  let text = ("hello, i'm Nurmukhamat!");
+//  text = text.split("").reverse().join("");
+//  console.log(text);
+ 
+//! Задача6
+// let Number = prompt("введите код");
+// for (let key = 0; key <= Number; key++) {
+//   console.log(key);
 // }
 
-// ! Задача8
-// const number = 7; 
-// if (number % 2 === 0) { 
-//              console.log('Число чётное');
-//  } else { 
-//             console.log('Число нечётное');
-//  }
-// Я могу объяснить эту ситуацию так: 2 процента номера или числа 7 строго равен 0,
-// то в консоль вышел бы чётное число, а если строго не равен то уже переходить к else.
+// let line = 7;
+// let line2 = "";
+// let line3 = "*";
+// for (let ts = 0; ts < line; ts++) {
+//   line2 += line3;
+//   console.log(line2);
+//   }
+//   let wl = "7";
+//   let res1 = "";
+//   let res2 = "#";
+//   let res3 = "0";
 
-// ! Задача 9
-// let num9 = 5;
-// num9 === 5 ? console.log("five") : console.log("not five");
+//   while (wl >= res3) {
+//     res1 += res2;
+//     console.log(res1);
+//     res3++
+//    }
+
+//! Задача7
+// var golem = "#";
+// while (golem.length<=7){
+//     console.log(golem + "\n");
+//     golem+="#";
+// }
+
+// function eren() {
+//     for (let naruto = "@"; naruto.length < 8; naruto += "@") {
+//       console.log(naruto);
+//     }
+//   }
+//   eren();
