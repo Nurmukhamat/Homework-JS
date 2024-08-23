7b4908816c132ba60fb98beb3bd3a5a939073980
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2024-08-23 16:25:33 +0300

    homework-JS-17

--- main.js
+++ main.js
@@ -1,45 +1,98 @@
-// !задача 1
-let user = "Экзамендин катышуучулары жана результаттары:";
-console.log(user);
+// ! Задача1
+// const num = prompt("enter your number!");
+// if (num == 10) {
+//     console.log(true)
+// } else  {
+//     console.log(false);
+// };
 
-let firstUserName = "Nooruz";
-let firstUserPoint = 97;
-let resultNooruzPoint = firstUserName + " - "+ firstUserPoint + "баллов из 100";
-console.log(resultNooruzPoint);
+// ! Задача2
+// const num2 = prompt("Введите номер квартиры!");
+// if (num2 > 0 && num2 <= 20) {
+//     console.log("Ваша квартира находится в 1 подъезде"); 
+// }
+//  else if (num2 > 20 && num2 <= 48) {
+//     console.log("Ваша квартира находится в 2 подъезде"); 
+// }
+//  else if (num2 > 48 && num2 <= 90) {
+//     console.log("Ваша квартира находится в 3 подъезде"); 
+// } else {
+//     console.log("Вы ошиблись квартирой!");
+// }
 
-let secondUserName = "Zhakshylyk";
-let secondUserPoint = 89;
-let resultZhakshylykPoint = secondUserName + " - " + secondUserPoint + " баллов из 100";
-console.log(resultZhakshylykPoint);
+// ! Задача3
+// const num3 = prompt("Введите ваше число");
+// if ( num3 > 0 && num3 < 5) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
 
-thirdUserName = "Alisher";
-thirdUserPoint = 75;
-let resultAlisherPoint = thirdUserName + " - " + thirdUserPoint + "баллов из 100";
-console.log(resultAlisherPoint);
+// !Задача4
+// const hours = prompt("Введите текущий час")
+// if (hours > 6 && hour <= 12) console.log("Доброй утро");
+// if (hours > 12 && hour <= 17) console.log("Доброе день");
+// if (hours > 17 && hour <= 24) console.log("Добрый ночи");
+// if (hours > 0 && hour <= 6) console.log("Добрый ночи");
 
+// ! Задача5
+// const hour = prompt("Введите текущий час");
+// const userName = prompt("Как вас зовут?");
+// if (hour > 6 && hour <= 12) console.log("Доброе утро " + userName);
+// if (hour > 12 && hour <= 17) console.log("Добрый день "+userName);
+// if (hour > 17 && hour <= 24) console.log("Доброй ночи "+ userName);
+// if (hour > 0 && hour <= 6) console.log("Доброй  ночи "+ userName);
 
-let fourthUserName = "Elizar";
-let fourthUserPoint = 90;
-let resultElizarPoint = fourthUserName + " - " + fourthUserPoint + "баллов из 100";
-console.log(resultElizarPoint);
+// ! Задача6
+// const x = prompt("Сколько вам лет?");
+// if (x <= 6 || x >= 65) {
+//     console.log("для пенсионерам и детям действует скида 50%!");
+// } else {
+//     console.log("Оплатите наличкой или картой?");
+// }
 
-let resultPoint = "Экзамендин орточо баллы:  87.75 балл";
-console.log(resultPoint);
+// !Задача7
+// let user1 = prompt("ИГРОК1) выберите вариант: 1)Камень, 2)Ножницы, 3)Бумага");
+// let user2 = prompt("ИГРОК2) выберите вариант: 1)Камень, 2)Ножницы, 3)Бумага");
+// if (user1 == 1 && user2 == 1) {
+//     console.log("ничья");
+// }
+// if (user1 == 1 && user2 == 2) {
+//     console.log("победил ИГРОК1");
+// }
+// if (user1 == 1 && user2 == 3) {
+//     console.log("Победил ИГРОК2");
+// }
+// if (user1 == 2 && user2 == 1) {
+//     console.log("Победил ИГРОК2");
+// }
+// if (user1 == 2 && user2 == 2) {
+//     console.log("ничья");
+// }
+// if (user1 == 2 && user2 == 3) {
+//     console.log("победил ИГРОК1");
+// }
+// if (user1 == 3 && user2 == 1) {
+//     console.log("победил ИГРОК1");
+// }
+// if (user1 == 3 && user2 == 2) {
+//     console.log("Победил ИГРОК2");
+// }
+// if (user1 == 3 && user2 == 3) {
+//     console.log("ничья");
+// }
 
-//!задача 2
+// ! Задача8
+// const number = 7; 
+// if (number % 2 === 0) { 
+//              console.log('Число чётное');
+//  } else { 
+//             console.log('Число нечётное');
+//  }
+// Я могу объяснить эту ситуацию так: 2 процента номера или числа 7 строго равен 0,
+// то в консоль вышел бы чётное число, а если строго не равен то уже переходить к else.
 
-let table = 4;
-let count = 10;
-   for (let m = 0; m < count; m++) {
-m++
-
-console.log(table, "X", m, "=", table * m+'');
---m
-}
-
-//! задача3
-let Name = prompt("Введите ваше имя");
-let x = alert("Вы успешно зарегистрировались!")
-let y = confirm("При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?") 
-    console.log(true)
-console.log(Name + " welcome to home!" );
+// ! Задача 9
+// let num9 = 5;
+// num9 === 5 ? console.log("five") : console.log("not five");
