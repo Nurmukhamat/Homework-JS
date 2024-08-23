aa19e4286176bb88efc05e6fa81ffa37a126c7b3
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2024-08-23 16:16:38 +0300

    Homework-JS-16

---
+++ main.js
@@ -0,0 +1,45 @@
+// !задача 1
+let user = "Экзамендин катышуучулары жана результаттары:";
+console.log(user);
+
+let firstUserName = "Nooruz";
+let firstUserPoint = 97;
+let resultNooruzPoint = firstUserName + " - "+ firstUserPoint + "баллов из 100";
+console.log(resultNooruzPoint);
+
+let secondUserName = "Zhakshylyk";
+let secondUserPoint = 89;
+let resultZhakshylykPoint = secondUserName + " - " + secondUserPoint + " баллов из 100";
+console.log(resultZhakshylykPoint);
+
+thirdUserName = "Alisher";
+thirdUserPoint = 75;
+let resultAlisherPoint = thirdUserName + " - " + thirdUserPoint + "баллов из 100";
+console.log(resultAlisherPoint);
+
+
+let fourthUserName = "Elizar";
+let fourthUserPoint = 90;
+let resultElizarPoint = fourthUserName + " - " + fourthUserPoint + "баллов из 100";
+console.log(resultElizarPoint);
+
+let resultPoint = "Экзамендин орточо баллы:  87.75 балл";
+console.log(resultPoint);
+
+//!задача 2
+
+let table = 4;
+let count = 10;
+   for (let m = 0; m < count; m++) {
+m++
+
+console.log(table, "X", m, "=", table * m+'');
+--m
+}
+
+//! задача3
+let Name = prompt("Введите ваше имя");
+let x = alert("Вы успешно зарегистрировались!")
+let y = confirm("При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?") 
+    console.log(true)
+console.log(Name + " welcome to home!" );
